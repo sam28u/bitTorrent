bf7d4bc3834126f8eea4509173c9a47e5a870b69
sam28u <sambhufirst11@gmail.com>
2026-08-19 00:29:36 +0530

    feat: implement Read function to parse incoming peer handshakes

--- handshake/handshake.go
+++ handshake/handshake.go
@@ -1,6 +1,10 @@
 package handshake
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
 
 type Handshake struct {
 	Pstr     string
@@ -24,4 +28,29 @@ func (h *Handshake) Serialize() []byte {
 	buf.Write(h.InfoHash[:])
 	buf.Write(h.PeerID[:])
 	return buf.Bytes()
+}
+
+func Read(r io.Reader) (*Handshake , error){
+	buf := make([]byte , 1)
+	_, err := io.ReadFull(r , buf)
+	if err != nil {
+		return nil , err
+	}
+	pstrlen := int(buf[0])
+	if pstrlen == 0{
+		return nil , fmt.Errorf("pstrlen length cannot be 0")
+	}
+	HandshakeBuf := make([]byte , pstrlen+48)
+	_, err = io.ReadFull(r , HandshakeBuf)
+	if err != nil {
+		return nil , err
+	}
+	var infoHash , peerID [20]byte
+	copy(infoHash[:] , HandshakeBuf[pstrlen + 8 : pstrlen + 28])
+	copy(peerID[:] , HandshakeBuf[pstrlen + 28 : pstrlen + 48])
+	return &Handshake{
+		Pstr: string(HandshakeBuf[0 : pstrlen]),
+		InfoHash: infoHash,
+		PeerID: peerID,
+	} , nil
 }
